91cf44491aee631a6fb641625b1bb3e2af71fd21
Santhoshcv19 <132180386+Santhoshcv19@users.noreply.github.com>
2023-11-25 03:01:41 +0530

    Update Datatable.jsx

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -18,7 +18,7 @@ const Datatable = ({columns}) => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`/${path}/${id}`);
+      await axios.delete(`https://booking-app-server-bdzt.onrender.com/api/${path}/${id}`);
       setList(list.filter((item) => item._id !== id));
     } catch (err) {}
   };
