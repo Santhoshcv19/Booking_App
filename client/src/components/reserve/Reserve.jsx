016057c55c930f07126a1d9d384293678ca0e155
Santhoshcv19 <132180386+Santhoshcv19@users.noreply.github.com>
2023-11-25 09:26:19 +0530

    Update Reserve.jsx

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom"
 
 const Reserve = ({ setOpen, hotelId }) => {
     const [selectedRooms, setSelectedRooms] = useState([])
-    const { data, loading, error } = useFetch(`https://booking-app-server-bdzt.onrender.com/api/rooms/${hotelId}`)
+    const { data, loading, error } = useFetch(`https://booking-app-server-bdzt.onrender.com/api/hotels/room/${hotelId}`)
     const { dates } = useContext(SearchContext);
 
     const getDatesInRange = (startDate, endDate) => {
